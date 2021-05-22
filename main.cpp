794e162790602e1f1416f7ea0645a0e48ff267f4
JamileFerino <>
2021-05-22 12:31:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//Vetor
+int numero [6];
+
+
+int main() {
+int a,b,c,d,e,f;
+
+
+cout<<"Digite um numero de 0 a 9:"<<endl;
+cin>>a;
+cout<<"Digite um numero de 0 a 9:"<<endl;
+cin>>b;
+cout<<"Digite um numero de 0 a 9:"<<endl;
+cin>>c;
+cout<<"Digite um numero de 0 a 9:"<<endl;
+cin>>d;
+cout<<"Digite um numero de 0 a 9:"<<endl;
+cin>>e;
+cout<<"Digite um numero de 0 a 9:"<<endl;
+cin>>f;
+}
