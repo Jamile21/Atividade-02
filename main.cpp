0d70ccdd9d5dcf023835d2513107d6445de5c0e4
Jamile Ferino <>
2021-05-23 20:02:16 +0000

    Max e Mim

--- main.cpp
+++ main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 using namespace std;
 
-//Vetor
-int numero [6];
+
+//Maximo e Minimo
+void mm (int *v, int num, int *min, int *max) {
+int i;
+*min = v[0];
+*max = v[0];
+for (i = 1; i < num; i++) {
+if (v[i] > *max) {
+*max = v[i];
+}
+else  {
+  if (v[i] < *min) {
+*min = v[i];
+     }
+   }
+  }
+}
 
 
 int main() {
-int a,b,c,d,e,f;
+int *vet;
+int num, i, minimo, maximo;
 
+// usuário determina o tamanho do vetor
+cout<<"Digite o tamanho do vetor:"<<endl;
+cin>>num; 
 
+vet = new int [num];
+
+for (i = 0; i < num; i++) {
 cout<<"Digite um numero de 0 a 9:"<<endl;
-cin>>a;
-cout<<"Digite um numero de 0 a 9:"<<endl;
-cin>>b;
-cout<<"Digite um numero de 0 a 9:"<<endl;
-cin>>c;
-cout<<"Digite um numero de 0 a 9:"<<endl;
-cin>>d;
-cout<<"Digite um numero de 0 a 9:"<<endl;
-cin>>e;
-cout<<"Digite um numero de 0 a 9:"<<endl;
-cin>>f;
+cin>> vet[i];
+   }
+
+mm(vet, num, &minimo, &maximo);
+
+cout<< "Max: " << maximo << " Mim: " << minimo;
+return 0;
+
 }
